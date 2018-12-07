261740fd3de7179de9fadc3742227ac1a230e201
Razil Tahir <razil.tahir@terang.asia>
2018-12-07 23:38:51 +0800

    Hey guys. I just added my code "library2". Please check and add anything or maybe suggest a better one.

--- library2.ts
+++ library2.ts
@@ -6,24 +6,38 @@ interface Library {
 }
 
 interface Book {
+    title : string,
+    author : string,
+    yearPublication : string,
+    publisher : string,
     lending : Lending
 }
 
 interface Staff {
+    name : string;
+    position : string;
+    address : string;
+    ic : string;
     membership? : Membership
 
 }
 
 interface Member {
+    ic : string,
+    name : string,
+    address : string,
     membership? : Membership
 }
 
 interface Membership {
+    memberId : string,
     lending : Lending,
     rent : Rent
 }
 
 interface Lending {
+    dateCheckin : string,
+    dateCheckout : string,
     fine : Fine
 }
 
